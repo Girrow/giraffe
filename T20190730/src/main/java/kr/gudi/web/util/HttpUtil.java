6d9a0cea195cca6cae7447c5de09e02468204ca8
harin <teri941227@gmail.com>
2019-08-07 00:46:43 +0900

    Exception - 처리

--- T20190730/src/main/java/kr/gudi/web/util/HttpUtil.java
+++ T20190730/src/main/java/kr/gudi/web/util/HttpUtil.java
@@ -1,5 +1,6 @@
 package kr.gudi.web.util;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
 
@@ -21,6 +22,7 @@ public class HttpUtil {
 			paramMap.put(name, value);
 		}
 		
+//		return checkParams(paramMap);
 		return paramMap;
 	}
 	
@@ -28,11 +30,38 @@ public class HttpUtil {
 		try {
 			res.setCharacterEncoding("UTF-8");
 			res.setContentType("application/json; charset=UTF-8");
-			JSONObject jObject = JSONObject.fromObject(resultMap);
+			
+			JSONObject jObject = JSONObject.fromObject(checkParams(resultMap));
 			res.getWriter().write(jObject.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static HashMap<String,Object> checkParams(HashMap<String,Object> params){
+		int totBlank=0;
+		int totNull=0;
+		HashMap<String,Object> errMap = new HashMap<String,Object>();
+		HashMap<String,Object> resultparams = new HashMap<String,Object>();
+		for(String key : params.keySet()) {
+			if(params.get(key)=="") {
+				resultparams.put(key, "empty");
+				System.out.println(key+"값이 비어있습니다.");
+				errMap.put(key,"비어있는 값");
+				totBlank++;
+			}else if(params.get(key)==null) {
+				resultparams.put(key, "null");
+				System.out.println(key+"값이 Null입니다.");
+				errMap.put(key,"null값");
+				totNull++;
+			}else {
+				resultparams.put(key, params.get(key));
+			}
+		}
+		errMap.put("blank 값",totBlank);
+		errMap.put("Null 값",totNull);
+		resultparams.put("err", errMap);
+		return resultparams;
 	}
 
 }
