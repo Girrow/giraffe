399bec7f0a3c8ea4c65d9c4044c4f6d6016959e2
harin <teri941227@gmail.com>
2019-08-16 00:11:37 +0900

    수정사항 추가

--- ha/src/main/resources/sql/input.sql
+++ ha/src/main/resources/sql/input.sql
@@ -3,15 +3,15 @@ CREATE TABLE `user` (
 	`id` VARCHAR(20) NOT NULL,
 	`pw` VARCHAR(30) NOT NULL,
 	PRIMARY KEY (`no`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDBspringbootspringboot
-;
+);
 
 CREATE TABLE `post`(
   `no` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   `postNo` INT,
-  `writer` VARCHAR(20spring),
-  `content` VARCHAR(50),
+  `writer` VARCHAR(20),
+  `contspringent` VARCHAR(50),
   `delYn` CHAR(1) DEFAULT 'N'
-)
+);
+
+INSERT INTO `user`(id,pw) values("harin","1234");
+INSERT INTO `user`(id,pw) values("admin","1234");
